5d655df8fbf000d58a88e5c17bbc6ae77657ffcb
Frederik <frederik.wagner@student.tugraz.at>
2020-10-02 16:27:19 +0200

    Added Navigation! Added Overview Fragment!

---
+++ app/src/main/java/com/example/passwordmanager/Overview.kt
@@ -0,0 +1,20 @@
+package com.example.passwordmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.passwordmanager.databinding.FragmentOverviewBinding
+
+class Overview : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = FragmentOverviewBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+}
